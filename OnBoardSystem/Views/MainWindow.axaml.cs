eb087a9e8f07b045879c0ea887adf4728029cc51
Zero-Point-Three Mach <45490638+Move2win@users.noreply.github.com>
2025-09-10 20:59:11 +0800

    Include necessary using for on-board system MainWindow.

--- OnBoardSystem/Views/MainWindow.axaml.cs
+++ OnBoardSystem/Views/MainWindow.axaml.cs
@@ -1,4 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Media;
+using Avalonia.Threading;
+using MsBox.Avalonia;
+using MsBox.Avalonia.Enums;
 
 namespace OnBoardSystem.Views
 {
