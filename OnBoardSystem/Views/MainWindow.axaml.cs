893d1c88516b18a1fa3faf6edf591e8ea930ccf4
Zero-Point-Three Mach <45490638+Move2win@users.noreply.github.com>
2025-09-11 16:25:41 +0800

    Cleanup MainWindow.axaml.cs

--- OnBoardSystem/Views/MainWindow.axaml.cs
+++ OnBoardSystem/Views/MainWindow.axaml.cs
@@ -1,26 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Xml;
 using Avalonia.Controls;
-using Avalonia.Interactivity;
-using Avalonia.Media;
-using Avalonia.Threading;
-using MsBox.Avalonia;
-using MsBox.Avalonia.Enums;
 
 namespace OnBoardSystem.Views
 {
     public partial class MainWindow : Window
     {
-        #region Variable initialization.
-        //LocomotiveSafetySystemFolder => "C:\Users\%username%\Documents\LocomotiveSafetySystem"
-        readonly string LocomotiveSafetySystemFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "LocomotiveSafetySystem");
-        //Current active view indicator
-        string CurrentView = "InitialView";
-        #endregion
-
         public MainWindow()
         {
             InitializeComponent();
