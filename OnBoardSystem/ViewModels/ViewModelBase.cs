afab9336205e810b0af6fa329d0d1309d59aa219
Zero-Point-Three Mach <45490638+Move2win@users.noreply.github.com>
2025-09-11 16:17:01 +0800

    Binding UI elements.

--- OnBoardSystem/ViewModels/ViewModelBase.cs
+++ OnBoardSystem/ViewModels/ViewModelBase.cs
@@ -1,8 +1,23 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace OnBoardSystem.ViewModels
 {
-    public class ViewModelBase : ObservableObject
+    public partial class ViewModelBase : ObservableObject
     {
+        [ObservableProperty]
+        private string txtDate = string.Empty;
+        [ObservableProperty]
+        private string txtTime = string.Empty;
+        [ObservableProperty]
+        private string txtSpeed = "0.0 KM/H";
+        [ObservableProperty]
+        private string txtAcceleration = "▲ 0.0 KM/H/sec";
+        [ObservableProperty]
+        private string txtLimiteSpeed = "********";
+        [ObservableProperty]
+        private string txtTrainNumber = "X0000";
+        [ObservableProperty]
+        private string txtDestination = "DESTINATION";
     }
 }
