ec4944c09b80e0410013ad69d5f6bbea5840ba5c
Zero-Point-Three Mach <45490638+Move2win@users.noreply.github.com>
2025-09-11 16:28:33 +0800

    Variable init and code cleanup.

--- OnBoardSystem/ViewModels/MainWindowViewModel.cs
+++ OnBoardSystem/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,22 @@
-﻿namespace OnBoardSystem.ViewModels
+﻿using System;
+using System.IO;
+using System.Threading;
+using CommunityToolkit.Mvvm.Input;
+
+namespace OnBoardSystem.ViewModels
 {
     public partial class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting { get; } = "Welcome to Avalonia!";
+        #region Variable initialization.
+        //LocomotiveSafetySystemFolder => "C:\Users\%username%\Documents\LocomotiveSafetySystem"
+        readonly string LocomotiveSafetySystemFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "LocomotiveSafetySystem");
+        //Current active view indicator
+        string CurrentView = "InitialView";
+
+        #endregion
+
+        public MainWindowViewModel()
+        {
+        }
     }
 }
